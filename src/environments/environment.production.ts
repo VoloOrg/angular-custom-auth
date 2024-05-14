c609deb1f9f801a0c18d9cf1cecca679c9e5fe73
karovolo <141014302+karovolo@users.noreply.github.com>
2024-05-14 17:46:12 +0400

    Update environment.production.ts

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://authentication-openiddict.azurewebsites.net',
+  baseUrl: 'https://authentication-openiddict-api.azurewebsites.net',
 };
